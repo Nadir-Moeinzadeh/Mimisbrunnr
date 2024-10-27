899bffeb370677bfedb4a1f316605448f2b8d04e
Nadir-Moeinzadeh <nader.moinzadeh@gmail.com>
2024-10-27 18:34:15 +0400

    email sender(sendgrid) added

--- Mimis.Utility/EmailSender.cs
+++ Mimis.Utility/EmailSender.cs
@@ -1,4 +1,16 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using SendGrid.Helpers.Mail;
+using SendGrid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Configuration;
+using SendGrid.Helpers.Mail;
+using SendGrid;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +18,28 @@ using System.Threading.Tasks;
 
 namespace Mimis.Utility
 {
-    internal class EmailSender
+    public class EmailSender : IEmailSender
     {
+        public string SendGridSecret { get; set; }
+
+        public EmailSender(IConfiguration _config)
+        {
+            SendGridSecret = _config.GetValue<string>("SendGrid:SecretKey");
+        }
+
+        public Task SendEmailAsync(string email, string subject, string htmlMessage)
+        {
+            //logic to send email
+
+            var client = new SendGridClient(SendGridSecret);
+
+            var from = new EmailAddress("nader.moinzadeh@gmail.com", "Mimisbrunnr");
+            var to = new EmailAddress(email);
+            var message = MailHelper.CreateSingleEmail(from, to, subject, "", htmlMessage);
+
+            return client.SendEmailAsync(message);
+
+
+        }
     }
 }
